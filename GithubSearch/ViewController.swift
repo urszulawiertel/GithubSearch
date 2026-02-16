1d01a17105d4e855e70f363535fb27e776d978dd
urszulawiertel <urszulawiertel@gmail.com>
2026-02-16 15:28:11 +0100

    Chore: initial project setup

---
+++ GithubSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubSearch
+//
+//  Created by Ula on 16/02/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,9 @@
+target 'GithubSearch' do
+  use_frameworks!
+
+pod 'SwiftLint'
+pod 'RxSwift', '6.6.0'
+pod 'RxCocoa', '6.6.0'
+pod 'SnapKit', '~> 5.7.0'
+
+end
